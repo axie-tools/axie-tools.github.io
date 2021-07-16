22d96a94a036cbfdad11acffbaa3ff9f79fc500a
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-07-16 13:46:39 -0300

    deploy 3

--- _nuxt/static/1626203307/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626203307",layout:"dashboard",error:a,state:{user:a,dashboardDrawerOpen:a,locales:["en",b],locale:b},serverRendered:true,routePath:c,config:{app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"es","\u002F"));
---
+++ _nuxt/static/1626453959/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fscholars","\u002Fcards","\u002Fbreading-cost"]})
