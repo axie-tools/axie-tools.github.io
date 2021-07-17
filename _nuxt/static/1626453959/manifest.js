506063fc1c9cf672e921f1cfe39cc98df6cac09e
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-07-17 12:08:52 -0300

    deploy 4

--- _nuxt/static/1626453959/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fscholars","\u002Fcards","\u002Fbreading-cost"]})
---
+++ _nuxt/static/1626534121/best-breeding/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/best-breeding", {data:[{}],fetch:{},mutations:[]});
