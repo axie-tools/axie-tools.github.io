fe3d1669872f27c1827d06972d89d3f698e8a593
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-07-18 19:36:40 -0300

    version 1.0.0

--- sw.js
+++ sw.js
@@ -2,4 +2,17 @@
 
 // https://github.com/NekR/self-destroying-sw
 
-console.log("version 1.0.0");
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
+    });
+});
