7460bfeb2298a86c7d6dea4c5b536f000059722f
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-07-26 22:48:00 -0300

    version 1.3.3

--- _nuxt/static/1627334563/pt/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627334563",layout:"default",error:a,state:{user:a,dashboardDrawerOpen:a,locales:["en","es"],locale:"pt"},serverRendered:true,routePath:"\u002Fpt",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null));
