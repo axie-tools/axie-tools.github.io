85986ce09b70f396eb52502732cc8526a7ad84f4
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-11-13 12:13:13 -0300

    v1.7.5

--- _nuxt/static/1636726591/season-19/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636726591",layout:"default",error:a,state:{dashboardDrawerOpen:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fseason-19",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null));
