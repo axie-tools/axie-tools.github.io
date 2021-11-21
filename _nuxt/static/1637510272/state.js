270141f7e444d70707e73c45221c182f57671a19
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-11-21 13:05:38 -0300

    v1.8.2

---
+++ _nuxt/static/1637510272/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637510272",layout:"default",error:a,state:{dashboardDrawerOpen:a,i18n:{routeParams:{}}},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null,"\u002F"));
