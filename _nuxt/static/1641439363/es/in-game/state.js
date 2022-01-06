2a6d3c7587fb9cbb265776487be4967e716c3ae3
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-01-06 00:27:33 -0300

    v1.9.4

--- _nuxt/static/1641439363/es/in-game/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641439363",layout:"default",error:a,state:{dashboardDrawerOpen:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fes\u002Fin-game",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null));
