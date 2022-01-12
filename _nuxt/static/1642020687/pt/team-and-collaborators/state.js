f63b6ebcfd82cb2e6f93d0c4f0a164d6fb7b9547
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-01-12 17:53:17 -0300

    v1.9.9

---
+++ _nuxt/static/1642020687/pt/team-and-collaborators/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642020687",layout:"default",error:a,state:{dashboardDrawerOpen:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fpt\u002Fteam-and-collaborators",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null));
