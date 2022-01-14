87e43c58d09bd7bddaf718d8663f10a56a2c7d02
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-01-14 00:17:31 -0300

    v1.10.0

---
+++ _nuxt/static/1642130191/pt/battle-history/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/pt/battle-history", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1642130191/pt/best-egg/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642130191",layout:"default",error:a,state:{dashboardDrawerOpen:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fpt\u002Fbest-egg",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null));
