29f356caf660bcd77d455981e0848e068d53a188
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-03-30 19:32:52 -0300

    v1.14.0

--- _nuxt/static/1648069280/pt/best-breeding/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648069280",layout:"default",error:a,state:{dashboardDrawerOpen:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fpt\u002Fbest-breeding",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null));
